53ce4c930fa50efef80cc5ff134dbc7cad07abb8
richard.frost <richard.frost@headforwards.com>
2016-10-17 11:16:55 +0100

    Updated background image to boats in Looe with correct aspect ratio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def get(event, context):
     print 'hello test'
     return {
-    'html': '<img class="max-height" src="https://www.headforwards.com/wp-content/themes/head-forwards/images/bg/looe-1.jpg"/>'
+    'html': '<img class="max-width" src="https://www.headforwards.com/wp-content/themes/head-forwards/images/bg/looe-1.jpg"/>'
     };
     pass
